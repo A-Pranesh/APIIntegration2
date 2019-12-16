3459f142cdf26bfdc4fd35a906f5b587b2a89349
A-Pranesh <48041549+A-Pranesh@users.noreply.github.com>
2019-12-16 11:22:13 +0530

    Add files via upload

---
+++ MarvelDetailsViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  MarvelDetailsViewModel.swift
+//  APIIntegration[Swift]
+//
+//  Created by Divum on 08/05/19.
+//  Copyright © 2019 Divum. All rights reserved.
+//
+
+import Foundation
+
+protocol MarvelDetailsViewModelDelegate: class {
+    func getResponse(_ response: [MarvelDetailsModel])
+}
+class MarvelDetailsViewModel {
+
+    weak var delegate: MarvelDetailsViewModelDelegate?
+
+    func getDetails() {
+        MarvelDetailsInterface.sharedInstance.getDetails { (result) in
+            self.delegate?.getResponse(result)
+        }
+        
+    }
+}
